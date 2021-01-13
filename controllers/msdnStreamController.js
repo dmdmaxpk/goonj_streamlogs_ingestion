6b90b4aacb461dd9177e249f4b2f819925d681b8
Muhammad Azam <muhammad.azam@dmdmax.com>
2021-01-13 17:24:16 +0500

    Data save in db - done

--- controllers/msdnStreamController.js
+++ controllers/msdnStreamController.js
@@ -1,26 +1,5 @@
 const mongoose = require('mongoose');
 const msdnStreamLog = mongoose.model('MsdnStreamLog');
-mongoose.set('useFindAndModify', false);	// To turn off findAndModify of Mongoose and use mongo native findOneAndUpdate
-const helper = require('../helper/helper');
-
-
-exports.post = async (req, res) => {
-
-	let postBody = req.body;
-
-	// Mongoose Query to findOneAndUpdate with upsert true
-	let options = { new: true, upsert: true, setDefaultsOnInsert: true }
-
-	// Mongoose Query to findOneAndUpdate condition
-	let insert_time = new Date(helper.setDate(new Date()).toString());
-	let condition = {"insert_time": insert_time, "platform": postBody.platform, "source": postBody.source}
-	console.log("condition: ", condition);
-
-	msdnStreamLog.findOneAndUpdate(condition, postBody, options)
-		.then(res => console.log(`Log added for: ${postBody.filename}`));
-
-	res.send("Log file parsed is done!");
-}
 
 // POST not consumed by any Svc (shifted to stream stats svc)
 exports.get = async (req, res) => {
@@ -41,3 +20,76 @@ exports.get = async (req, res) => {
 	else
 		res.send({status: 404, message: 'Data not found'});
 }
+
+exports.post = async (req, res) => {
+
+	let body = req.body;
+	let records, objKey, channels, channel, counter = 0 ;
+	for (records of body.data) {
+		for (objKey in records) {
+			if (records.hasOwnProperty(objKey) && objKey !== 'msdn') {
+				channels = records[objKey];
+				if (channels.length > 0){
+					for (channel of channels){
+						await this.insertOrUpdateRecord(req, objKey, records['msdn'], channel);
+						counter = counter + 1;
+					}
+				}
+			}
+		}
+	}
+
+
+	console.log('counter: ', counter)
+	res.send("Log file parsed is done!");
+}
+
+exports.insertOrUpdateRecord = async (req, objKey, msdn, record) =>{
+
+	let body = req.body;
+	let platform = body.platform;
+	let source = body.source;
+	let filename = body.filename;
+
+	// Condition for update or insert a record
+	let condition = {"msdn": msdn, "logDate": record.date, "logHour": record.hour, "platform": platform, "source": source, "computeSource": objKey}
+
+	let postBody = {
+		platform: platform, // Web OR Android
+		source: source,   // live channel OR Vod
+		msdn: msdn, // user msdn
+		computeSource: objKey, // channelName or fileName
+		logDate: record.date, // Date from log file
+		logHour: record.hour, // Hour from log file
+		bitrateCount: record.bitrateCount, // Total bitrate count
+		viewCount: record.viewCount, // Total view count
+		filename: filename, // Parsed log file name
+	};
+
+	let result = await this.show(condition);
+	if (result){
+		await this.update(result, postBody);
+		return;
+	}
+
+	return await this.save(postBody);
+}
+
+exports.show = async (condition) => {
+
+	return await msdnStreamLog.findOne(condition);
+}
+
+exports.update = async (result, postBody) => {
+
+	let bitrateCount = Number(postBody.bitrateCount) + Number(result.bitrateCount);
+	let viewCount = Number(postBody.viewCount) + Number(result.viewCount);
+	return await msdnStreamLog.updateOne({"_id": result._id},{$set:{bitrateCount: bitrateCount, viewCount: viewCount}});
+}
+
+exports.save = async (postBody) => {
+
+	let msdnStreamLogObj = new msdnStreamLog(postBody);
+	return await msdnStreamLogObj.save();
+}
+
--- models/MsdnStreamLog.js
+++ models/MsdnStreamLog.js
@@ -4,11 +4,16 @@ const {Schema} = mongoose;
 
 const MsdnStreamSchema = new Schema({
     _id: { 'type': String, 'default': shortid.generate },
-    filename: String,
-    platform: String,   // Web OR Android
+    platform: String, // Web OR Android
     source: String,   // live channel OR Vod
-    data: Array,
-    insert_time: { type: Date, default: Date.now }
+    msdn: String, // user msdn
+    computeSource: String, // channelName or fileName
+    logDate: String, // Date from log file
+    logHour: String, // Hour from log file
+    bitrateCount: Number, // Total bitrate count
+    viewCount: Number, // Total view count
+    filename: String, // Parsed log file name
+    insertTime: { type: Date, default: Date.now }
 }, { strict: true })
 
 module.exports = mongoose.model('MsdnStreamLog', MsdnStreamSchema);
